6dc382dee26f0aa06725b87fd0f5fabc2e8f284c
Mike Vorontsov <mi17@umbc.edu>
2016-08-08 23:11:51 -0400

    Added different colors to the balls

--- PPPROJECT/Assets/loadBalls.cs
+++ PPPROJECT/Assets/loadBalls.cs
@@ -20,8 +20,21 @@ public class loadBalls : MonoBehaviour {
         		//GameObject myObject = Resources.Load("Ball") as GameObject;
         		for (int i = 0; i < n; i++){
                     s = GameObject.Instantiate(Resources.Load("Orb")) as GameObject;
-            		s.transform.position = new Vector3 ((Random.value*10)-5, (Random.value*10)-5, 0F);
+					s.transform.position = new Vector3 ((Random.value*3)-1.5f, (Random.value*2)-1+1, (Random.value*3)-1.5f);
         			s.name = "item" + itemData[i]["id"] + ":" +itemData[i]["first_name"];
+					GameObject child = s.transform.FindChild("Text").gameObject;
+					child.GetComponent<TextMesh>().text = " "+itemData[i]["first_name"];
+					
+			Color c;
+			if (i % 2 == 0) {
+				c = Color.green;
+			} else {
+				c = Color.red;
+			}
+			Renderer r = s.GetComponent<Renderer> ();
+			r.material.SetColor ("_SpecColor", c);
+			r.material.SetColor ("_EmissionColor", c);
+			s.GetComponent<Light> ().color = c;
         		}
     }
 
