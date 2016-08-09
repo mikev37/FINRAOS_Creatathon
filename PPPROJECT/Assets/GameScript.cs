5d1f4daa41da0861f2bd8ffc46284b1609826738
Wang, Hannah <hywang.pitts@gmail.com>
2016-08-08 23:19:52 -0400

    Generate movements for sepheres

--- PPPROJECT/Assets/GameScript.cs
+++ PPPROJECT/Assets/GameScript.cs
@@ -4,8 +4,10 @@ using System.IO;
 using LitJson;
 using System.Data;
 using Mono.Data.SqliteClient;
+using System.Collections.Generic;
 
-public class GameScript : MonoBehaviour {
+public class GameScript : MonoBehaviour
+{
 	private GameObject s;
 	private string jsonString;
 	private JsonData itemData;
@@ -14,58 +16,89 @@ public class GameScript : MonoBehaviour {
 	private IDbCommand dbCommand;
 	private IDataReader dataReader;
 
-//	public Transform startMarker;
-//	public Transform endMarker;
-//	public float speed = 1.0F;
-//	private float startTime;
-//	private float journeyLength;
+	public Transform startPosition;
+	public Transform endPosition;
+	public List<Vector3> positions;
+	public float startTime;
+	public float totalDistanceToDestination;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		// read from database
-		Debug.Log(Application.dataPath);
-		string connStr = "URI=File:"+Application.dataPath+"/test_1.db";
-
-
-		dbConn = new SqliteConnection (connStr);
-
-		dbConn.Open();
-
-		string query = "select * from user";
-
-		dbCommand = dbConn.CreateCommand();
-		dbCommand.CommandText = query;
-		dataReader = dbCommand.ExecuteReader ();
-
-		while (dataReader.Read ()) {
-			Debug.Log(dataReader.GetString(1) + " " );
-		}
-			
-		dbConn.Close();
-
-
-
-//		jsonString = File.ReadAllText(Application.dataPath + "/MOCK_DATA.json");
-//		Debug.Log (jsonString);
+//		Debug.Log(Application.dataPath);
+//		string connStr = "URI=File:"+Application.dataPath+"/test_1.db";
 //
-//		itemData = JsonMapper.ToObject (jsonString);
+//		dbConn = new SqliteConnection (connStr);
 //
-//		int n = itemData.Count;
+//		dbConn.Open();
 //
-//		for (int i = 0; i < n; i++){
-//			s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//			s.transform.position = new Vector3 ((Random.value*10)-5, (Random.value*10)-5, 0F);
-//			s.name = "item" + itemData[i]["id"] + ":" +itemData[i]["first_name"];
+//		string query = "select * from user_2";
+//
+//		dbCommand = dbConn.CreateCommand();
+//		dbCommand.CommandText = query;
+//		dataReader = dbCommand.ExecuteReader ();
+//
+//		while (dataReader.Read ()) {
+//			Debug.Log(dataReader.GetString(1) + " " + dataReader.GetString(0));
 //		}
+//			
+//		dbConn.Close();
 
-//		startTime = Time.time;
-//		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+		//Read data from json file
+		jsonString = File.ReadAllText (Application.dataPath + "/MOCK_DATA.json");
+		Debug.Log (jsonString);
+
+		itemData = JsonMapper.ToObject (jsonString);
+
+		int n = itemData.Count;
+		positions = new List<Vector3> ();
+
+		for (int i = 0; i < n; i++) {
+			s = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			s.transform.position = new Vector3 ((Random.value * 10) - 5, (Random.value * 10) - 5, 0F);
+			//s.name = "item" + itemData[i]["id"] + ":" +itemData[i]["first_name"];
+			s.name = itemData[i][1] + " ";
+			Debug.Log (s.name);
+
+			if (itemData [i].Count > 0) {
+				Debug.Log (itemData [i].Count);
+				for (int j = 0; j < itemData [i].Count; j++) {
+					GameObject childSphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+					childSphere.transform.position = s.transform.position;
+					
+					positions.Add (new Vector3 (Random.value * 2, Random.value * 2, Random.value * 2));
+				}
+			}
+
+			//use Coroutine instead of Update() to schedule movement
+
+		}
+		//StartCoroutine (MultipleLerp (1, positions));
 	}
-	
+
+
+	IEnumerator MultipleLerp (float speed, List<Vector3> positions) {
+		foreach (Vector3 position in positions) {
+			Vector3 startPos = transform.position;
+			float timer = 0f;
+			while (timer <= 1f) {
+				timer += Time.deltaTime * speed;
+				Vector3 newPosition = Vector3.Lerp (startPos, position, timer);
+				transform.position = newPosition;
+				yield return new WaitForEndOfFrame ();
+			}
+			transform.position = position;
+			startPos = position;
+		}
+		yield return false;
+	}
+		
 	// Update is called once per frame
-	void Update () {
-//		float distCovered = (Time.time - startTime) * speed;
-//		float fracJourney = distCovered / journeyLength;
-//		transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+	void Update (){
+		if(Input.GetKeyDown("space")){
+			MultipleLerp (1, positions);
+		}
+		
 	}
 }
