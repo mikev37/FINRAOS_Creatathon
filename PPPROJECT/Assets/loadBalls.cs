dbd7bfa5713ad4a2cbfd5abc06876e5bb5fa7bad
Mike Vorontsov <mi17@umbc.edu>
2016-08-08 22:51:59 -0400

    Now can instantiate Orbs

---
+++ PPPROJECT/Assets/loadBalls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+using System.Data;
+using Mono.Data.SqliteClient;
+
+public class loadBalls : MonoBehaviour {
+    private GameObject s;
+    private string jsonString;
+    private JsonData itemData;
+    // Use this for initialization
+    void Start () {
+        		jsonString = File.ReadAllText(Application.dataPath + "/MOCK_DATA.json");
+        		Debug.Log (jsonString);
+        
+        		itemData = JsonMapper.ToObject (jsonString);
+        
+        		int n = itemData.Count;
+        		//GameObject myObject = Resources.Load("Ball") as GameObject;
+        		for (int i = 0; i < n; i++){
+                    s = GameObject.Instantiate(Resources.Load("Orb")) as GameObject;
+            		s.transform.position = new Vector3 ((Random.value*10)-5, (Random.value*10)-5, 0F);
+        			s.name = "item" + itemData[i]["id"] + ":" +itemData[i]["first_name"];
+        		}
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
